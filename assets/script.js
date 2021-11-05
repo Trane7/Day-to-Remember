5f065585cb9a3b1f62ff745e193931e76f8a75ca
Dee <dgrowette@gmail.com>
2021-11-05 10:44:36 -0700

    added save button and display for 5 am

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,64 @@
 var time = moment()
+
+
+$("#currentDay").text(moment().format('MMMM Do YYYY, HH:mm tt'));
+
+
+
+
+$(document).ready( function() {
+    colorChange ();
+    renderText();
+});
+
+
+function colorChange () {
+    
+    var currentTime = moment().hours();
+    console.log("Current Time" + currentTime);
+
+$(".input").each(function () {
+    var scheduledTime = parseInt($(this).attr("id"));
+    console.log(scheduledTime);
+
+    if (currentTime > scheduledTime) {
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+        $(this).addClass("past");
+    } else if (currentTime < scheduledTime) {
+        $(this).removeClass("present");
+        $(this).removeClass("past");
+        $(this).addClass("future");
+    } else {
+        $(this).removeClass("future");
+        $(this).removeClass("past");
+        $(this).addClass("present");
+    }
+  });
+}
+
+
 var eventInput
 var eventTime
 
-$("#currentDay").text(time).format('MMMM Do YYYY, HH:mm tt');
+$(".saveBtn").click(function() {
+    eventInput = $(this).siblings(".input").val();
+    console.log(eventInput);
+    eventTime = $(this).siblings(".hour").text();
+    console.log(eventTime);
+    localStorage.setItem(eventTime, JSON.stringify(eventInput));
 
-eventInput = $(this).siblings()
+    colorChange ();
+    renderText ();
+})
+
+
+
+
+
+function renderText () {   
+    var saveEventInput5 = JSON.parse(localStorage.getItem("5:00 AM"));
+    $("#5").val("");
+    $("#5").val(saveEventInput5);
+
+}
